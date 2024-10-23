7df8e5da7116d199991641efe06c54c94893ef79
Shailendra Lodhi <55943308+WackyCodes@users.noreply.github.com>
2024-10-23 15:07:24 +0530

    Create sharedPref.kt

---
+++ sharedPref.kt
@@ -0,0 +1,7 @@
+val sharedPreferences = EncryptedSharedPreferences.create(
+    "secure_prefs",
+    MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC),
+    context,
+    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+)
